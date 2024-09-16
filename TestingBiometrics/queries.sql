91e933e46661599197e47df94f57c458923f5b07
JackStandley <89469516+JackStandley@users.noreply.github.com>
2024-09-15 19:13:38 -0500

    Update queries.sql
    adds '--' as a separator

--- TestingBiometrics/queries.sql
+++ TestingBiometrics/queries.sql
@@ -1,10 +1,11 @@
+-- CREATE_TYPING_METRICS_TABLE
 CREATE TABLE IF NOT EXISTS TypingMetrics (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     average_wpm REAL,
     average_time_between_keystrokes REAL
 );
 
--- Create table for key pair times
+-- CREATE_KEY_PAIR_TIMES_TABLE
 CREATE TABLE IF NOT EXISTS KeyPairTimes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     key_pair TEXT,
@@ -12,28 +13,28 @@ CREATE TABLE IF NOT EXISTS KeyPairTimes (
     count INTEGER
 );
 
--- Select existing averages from TypingMetrics
+-- SELECT_EXISTING_AVERAGES
 SELECT COUNT(*), AVG(average_wpm), AVG(average_time_between_keystrokes)
 FROM TypingMetrics;
 
--- Insert new averages into TypingMetrics
+-- INSERT_NEW_AVERAGES
 INSERT INTO TypingMetrics (average_wpm, average_time_between_keystrokes)
 VALUES (?, ?);
 
--- Update existing averages in TypingMetrics
+-- UPDATE_EXISTING_AVERAGES
 UPDATE TypingMetrics
 SET average_wpm = ?, average_time_between_keystrokes = ?;
 
--- Select existing key pair data
+-- SELECT_EXISTING_KEY_PAIR_DATA
 SELECT average_time, count
 FROM KeyPairTimes
 WHERE key_pair = ?;
 
--- Insert new key pair data
+-- INSERT_NEW_KEY_PAIR_DATA
 INSERT INTO KeyPairTimes (key_pair, average_time, count)
 VALUES (?, ?, ?);
 
--- Update existing key pair data
+-- UPDATE_EXISTING_KEY_PAIR_DATA
 UPDATE KeyPairTimes
 SET average_time = ?, count = ?
 WHERE key_pair = ?;
