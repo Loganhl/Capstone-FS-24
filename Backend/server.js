16f5a7b5d6087dd2c5eecebce7a3b31e94693b05
gar7mn <gar7mn@missouri.edu>
2024-10-21 19:50:23 -0500

    added endpoints to post data for backend

--- Backend/server.js
+++ Backend/server.js
@@ -5,10 +5,10 @@ const PORT = 2500
 const cors = require('cors')
 const connection = mysql.createConnection({
 
-  "user":"root",
+  "user": "root",
   "password":"secretsquirrels",
   "database":"biometric_auth",
-  "host":"mysql",
+  "host":"http://mysql:3306",
 })
 app.use(cors())
 app.use(express.json())
@@ -17,15 +17,45 @@ app.get('/api/users',(req,res)=>{
     res.json(result);
   })
 })
-
+app.post('/api/mousedata:userid',(req,res)=>{
+  if (req.params.userid != null) {
+    let data_id = req.body.data_id;
+    let user_id = req.params.userid;
+    let avg_speed = req.body.avg_speed;
+    let avg_click_dwell = req.body.avg_click_dwell;
+    let avg_double_click = req.body.avg_double_click;
+    let query = 'INSERT INTO MouseData (data_id,user_id,avg_speed,avg_click_dwell,avg_double_click) VALUES (?,?,?,?);'
+    connection.query(query,[data_id,user_id,avg_speed,avg_click_dwell.avg_double_click],(err,result,fields)=>{
+      res.json(result);
+    })
+  }
+})
+app.post('/api/keyboarddata:userid',(req,res)=>{
+    if (req.params.userid != null) {
+      let data_id = req.body.data_id
+      let user_id = req.body.user_id
+      let wpm = req.body.wpm
+      let keystrokes_per_10_sec = req.body.keystrokes_per_10_sec
+      let keys_per_second = req.body.keys_per_second;
+      let query = "INSERT into KeyboardData (data_id,user_id,wpm,keystrokes_per_10_sec,keys_per_second) VALUES (?,?,?,?,?);"
+      connection.query(query,[data_id,user_id,wpm,keystrokes_per_10_sec,keys_per_second],(err,result,fields)=>{
+        res.json(result);
+      })
+      res.json("succseffully logged anomaly")
+    }
+    if(req.params.userid == null){
+      res.json({"expected user_id parameter":"got none"})
+    }
+})
 app.get('/api/activeusers',(req,res)=>{
-  if (req.params.id != null) {
-    connection.query("SELECT a.USERNAME b.USER_ID FROM USER_ENTITY a OFFLINE_USER_SESSION b WHERE a.ID = b.USER_ID",(err,result,fields)=>{
+  
+    connection.query("SELECT a.USERNAME b.USER_ID FROM USER_ENTITY a, OFFLINE_USER_SESSION b WHERE a.ID = b.USER_ID",(err,result,fields)=>{
       res.json(result);   
     })
-    res.send("hello");
-  }
-  res.send("hello");
+
 })
 
+app.get('/api/keyboarddata:userid',(req,res)=>{
+  //may end up deleting this one
+})
 app.listen(PORT,console.log(`app listening on port ${PORT}`))
