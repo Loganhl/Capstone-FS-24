703a05d1b4cf7791d3727e75e6015d4aec0eb206
JackStandley <jspr5@umsystem.edu>
2024-11-02 12:49:36 -0500

    Converted SQLlite to MySQL

--- BioVaultDataCollection/main.py
+++ BioVaultDataCollection/main.py
@@ -1,4 +1,4 @@
-import sqlite3
+import mysql.connector
 import time
 from metrics.keystroke_metrics import KeystrokeMetrics
 from metrics.mouse_metrics import MouseMetrics
@@ -6,8 +6,15 @@ from utils.database import setup_database, insert_metric
 from pynput import keyboard, mouse
 import os
 
+# Database connection parameters
+config = {
+    'user': 'mashedsnake',         
+    'password': 'ilovelamp',     
+    'host': 'mysql',             
+    'database': 'biometric_auth'     
+}
 
-def collect_metrics(keystroke_metrics, mouse_metrics):
+def collect_metrics(keystroke_metrics, mouse_metrics, cursor):
     # Collects key presses
     def on_key_press(key):
         keystroke_metrics.on_press(key)
@@ -53,23 +60,22 @@ def collect_metrics(keystroke_metrics, mouse_metrics):
     finally:
         listener_k.stop()
         listener_m.stop()
-        conn.close()
-
-
-db_path = os.path.join(os.path.dirname(__file__), 'data', 'biometric_data.db')
-
-setup_database(db_path)
-
-
-try:
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    print("Database connection established successfully.")
-except sqlite3.OperationalError as e:
-    print(f"Error: {e}")
-    exit(1)
 
 if __name__ == "__main__":
+    setup_database()  # Set up the database
+
+    # Create a connection to MySQL
+    try:
+        conn = mysql.connector.connect(**config)
+        cursor = conn.cursor()
+        print("Database connection established successfully.")
+    except mysql.connector.Error as e:
+        print(f"Error: {e}")
+        exit(1)
+
     keystroke_metrics = KeystrokeMetrics()
     mouse_metrics = MouseMetrics()
-    collect_metrics(keystroke_metrics, mouse_metrics)
+    collect_metrics(keystroke_metrics, mouse_metrics, cursor)
+
+    # Close the connection at the end of the program
+    conn.close()
