289f3ff36a4e235b5b5feac5a3c270b1857e2847
Kaelyn Crews <crewskaelyn@gmail.com>
2024-11-05 20:58:22 -0600

    adding light mode/dark mode

--- Frontend/src/components/navigation.jsx
+++ Frontend/src/components/navigation.jsx
@@ -1,44 +1,71 @@
-import React from "react";
-import { Button,Navbar,Nav, Container } from "react-bootstrap";
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Navbar, Nav, Container, Form } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "../pages/home";
 import MyBiometrics from "../pages/UserBiometrics";
 import Dashboard from "../pages/dashboard";
 import Login from "../pages/login";
 
-const Navigation = ({token,client})=>{
+const Navigation = ({ token, client }) => {
 
-    return(
+    const [theme, setTheme] = useState('light');
+
+    useEffect(() => {
+        document.body.className = theme;
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+
+    }
+  return (
     <Router>
-        <Navbar  fixed="top" expand='lg' className="bg-body-tertiary">
-            <Container>
-
-            <Nav variant="tabs" defaultActiveKey='/'>
-                <Navbar.Brand href="">Biovault</Navbar.Brand>
-                <Nav.Item>
-                    <Nav.Link as={Link} to='/'>Home</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link as={Link} to='/biometrics'>My Biometrics</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link as={Link} to='/dashboard'>Admin Dashboard</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link onClick={()=> client.logout()}>Log out</Nav.Link>
-                </Nav.Item>
-                <Navbar.Toggle >Light</Navbar.Toggle>
-            </Nav>
-            </Container>
-            </Navbar>
-            <Routes>
-                <Route exact path="/"  element={<Home/>}/>
-                <Route path="/biometrics" element={<MyBiometrics token={token} client={client} />}/>
-                <Route path="/dashboard" element={<Dashboard/>}/>
-                
-            </Routes>
-        </Router>
-        )
-}
+      <Navbar fixed="top" expand="lg" className={`navbar-${theme} bg-body-${theme}`} variant={theme === 'light' ? 'light' : 'dark'}>
+        <Container>
+          <Nav variant="tabs" defaultActiveKey="/">
+            <Navbar.Brand href="">Biovault</Navbar.Brand>
+            <Navbar.Collapse>
+              <Nav.Item>
+                <Nav.Link as={Link} to="/">
+                  Home
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link as={Link} to="/biometrics">
+                  My Biometrics
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link as={Link} to="/dashboard">
+                  Admin Dashboard
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link onClick={() => client.logout()}>Log out</Nav.Link>
+              </Nav.Item>
+              <Form.Check
+                type = "switch"
+                id="theme-switch"
+                label="Dark Mode"
+                checked={theme === 'dark'}
+                onChange={toggleTheme}
+                className="ms-3"
+                />
+            </Navbar.Collapse>
+            <Navbar.Toggle>Collapse</Navbar.Toggle>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route
+          path="/biometrics"
+          element={<MyBiometrics token={token} client={client} />}
+        />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default Navigation;
