8d57c004d18be838f28f2b0db24dca643557b5da
JackStandley <jspr5@umsystem.edu>
2024-11-17 22:01:49 -0600

    Added metric count checks so calculations will not be based off of small amounts of metrics, also updated mouse speed to exponential moving average

--- BioVaultDataCollection/database/database.py
+++ BioVaultDataCollection/database/database.py
@@ -24,14 +24,16 @@ class Database:
             print(f"Error connecting to the database: {e}")
             self.conn = None
 
-    def insert_metric(self, table_name, value, user_id):
-
+    def insert_metric(self, table_name, value, user_id, min_threshold=0.01):
         user_id = get_userid('user@user.com', 'user')
         if self.conn is None:
             print("Database connection not established. Cannot insert data.")
             return
+        if value <= min_threshold:
+            print(f"Value {value} below threshold {min_threshold}. Skipping insertion into {table_name}.")
+            return
         try:
-            query = f"INSERT INTO {table_name} (value, USER_ID) VALUES (%s,%s)"
+            query = f"INSERT INTO {table_name} (value, USER_ID) VALUES (%s, %s)"
             self.cursor.execute(query, (value, user_id))
             self.conn.commit()
             print(f"Inserted value {value} into table {table_name}.")
@@ -45,3 +47,9 @@ class Database:
         if self.conn:
             self.conn.close()
             print("Database connection closed.")
+#git push origin main lol
+
+
+# the best way to have all the fun in the world is to eat cookies
+
+#the best way to have all th efun in the world is to eat all the cookies possible tj
