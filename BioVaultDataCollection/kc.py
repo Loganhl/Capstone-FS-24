98642677952fa3cff37ab9b9734f762eb8d31af3
gar7mn <gar7mn@missouri.edu>
2024-11-27 10:52:08 -0600

    add error handling to keycloak auth.

--- BioVaultDataCollection/kc.py
+++ BioVaultDataCollection/kc.py
@@ -1,9 +1,16 @@
-from keycloak import keycloak_openid
+from keycloak import keycloak_openid,KeycloakConnectionError,KeycloakAuthenticationError
 # returns the user id whne passed to to database
 def get_userid(username,password):
-    kcoid= keycloak_openid.KeycloakOpenID('http://localhost:8080','biovault','biovault-client',client_secret_key='fy0OiDrL2BQ7zX26qdkywVrEdoiYHj9r')
-    token = kcoid.token(username,password,grant_type='password')
-    userinfo = kcoid.userinfo(token['access_token'])
-    userid = userinfo['sub']
-    return userid
+    try:
+        kcoid= keycloak_openid.KeycloakOpenID('http://localhost:8080','biovault','biovault-client',client_secret_key='fy0OiDrL2BQ7zX26qdkywVrEdoiYHj9r')
+        token = kcoid.token(username,password,grant_type='password')
+        userinfo = kcoid.userinfo(token['access_token'])
+        userid = userinfo['sub']
+        return userid
+    except KeycloakConnectionError:
+        print('Error Connecting to Keycloak')
+        exit(1)
 
+    except KeycloakAuthenticationError:
+        print('Invalid Username or Password')
+        exit(1)
